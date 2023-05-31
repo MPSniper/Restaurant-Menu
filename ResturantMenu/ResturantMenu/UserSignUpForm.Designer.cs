fe77465330c3847e709d37b8d7485c0a9bd6c7d6
Kiana-Kiani <kiana8kiani@gmail.com>
2023-05-31 20:54:15 +0330

    create User Sign Up Page

---
+++ ResturantMenu/ResturantMenu/UserSignUpForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ResturantMenu
+{
+    partial class UserSignUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            groupBox1 = new GroupBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            listBox1 = new ListBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(250, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 15);
+            label1.TabIndex = 0;
+            label1.Text = ":نام";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(199, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 15);
+            label2.TabIndex = 1;
+            label2.Text = ":نام خانوادگی ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(225, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 2;
+            label3.Text = ":کد ملی ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(237, 194);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 3;
+            label4.Text = ":آدرس";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(223, 46, 56);
+            groupBox1.Controls.Add(listBox1);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Location = new Point(224, 24);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(349, 387);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(93, 156, 89);
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(52, 320);
+            button1.Name = "button1";
+            button1.Size = new Size(250, 50);
+            button1.TabIndex = 4;
+            button1.Text = " ثبت نام";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(199, 232, 202);
+            textBox1.Location = new Point(79, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(194, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(199, 232, 202);
+            textBox2.Location = new Point(80, 101);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(192, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(199, 232, 202);
+            textBox3.Location = new Point(82, 159);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(190, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.FromArgb(199, 232, 202);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(80, 215);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(194, 94);
+            listBox1.TabIndex = 8;
+            // 
+            // UserSignUpForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(93, 156, 89);
+            ClientSize = new Size(851, 585);
+            Controls.Add(groupBox1);
+            Name = "UserSignUpForm";
+            Text = "UserSignUpForm";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private GroupBox groupBox1;
+        private ListBox listBox1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
