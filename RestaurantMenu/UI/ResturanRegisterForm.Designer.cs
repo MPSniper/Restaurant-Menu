b4e818d319eb03f0bc48dc737b5b933137f0fa77
M Programmer <mohammadkoohi@outlook.com>
2023-06-05 14:29:37 +0330

    UI Froms Added

---
+++ RestaurantMenu/UI/ResturanRegisterForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace UI
+{
+    partial class ResturanRegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnBack = new System.Windows.Forms.Label();
+            this.BtnLogin = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.panel1.Size = new System.Drawing.Size(470, 345);
+            this.panel1.TabIndex = 20;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Controls.Add(this.BtnBack);
+            this.panel2.Controls.Add(this.BtnLogin);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(3, 56);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(456, 282);
+            this.panel2.TabIndex = 3;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown2.Location = new System.Drawing.Point(262, 219);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(44, 41);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.Location = new System.Drawing.Point(358, 220);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 41);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(262, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 41);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox4.Location = new System.Drawing.Point(3, 47);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(243, 129);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(262, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 41);
+            this.textBox2.TabIndex = 2;
+            // 
+            // BtnBack
+            // 
+            this.BtnBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnBack.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnBack.Image = global::UI.Properties.Resources.icons8_back_to_40px_1;
+            this.BtnBack.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnBack.Location = new System.Drawing.Point(3, 233);
+            this.BtnBack.Name = "BtnBack";
+            this.BtnBack.Size = new System.Drawing.Size(243, 43);
+            this.BtnBack.TabIndex = 0;
+            this.BtnBack.Text = "بازگشت";
+            this.BtnBack.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnBack.Click += new System.EventHandler(this.BtnBack_Click);
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLogin.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnLogin.Image = global::UI.Properties.Resources.icons8_asian_street_food_40px;
+            this.BtnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnLogin.Location = new System.Drawing.Point(3, 180);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(243, 43);
+            this.BtnLogin.TabIndex = 0;
+            this.BtnLogin.Text = "ثبت اطلاعات";
+            this.BtnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(262, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 41);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "نام رستوران : ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 41);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "آدرس : ";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label8.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label8.Location = new System.Drawing.Point(312, 219);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 41);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "تا : ";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label7.Location = new System.Drawing.Point(408, 220);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 41);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "از : ";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.Location = new System.Drawing.Point(262, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 41);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "شیفت کاری : ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(262, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 41);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "صاحب رستوران : ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label6.Location = new System.Drawing.Point(3, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(456, 41);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "اطلاعات رستوران";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ResturanRegisterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UI.Properties.Resources._45_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(491, 369);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("B Zar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ResturanRegisterForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label BtnLogin;
+        private Panel panel2;
+        private TextBox textBox1;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private Label BtnBack;
+        private Label label1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label6;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown1;
+        private Label label8;
+        private Label label7;
+    }
+}
---
+++ RestaurantMenu/UI/SplashForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace UI
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Label();
+            this.BtnResturanRegister = new System.Windows.Forms.Label();
+            this.BtnMainResturan = new System.Windows.Forms.Label();
+            this.BtnMainUser = new System.Windows.Forms.Label();
+            this.BtnUserRegister = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.BtnExit);
+            this.panel1.Controls.Add(this.BtnResturanRegister);
+            this.panel1.Controls.Add(this.BtnMainResturan);
+            this.panel1.Controls.Add(this.BtnMainUser);
+            this.panel1.Controls.Add(this.BtnUserRegister);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(525, 316);
+            this.panel1.TabIndex = 20;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnExit.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnExit.Image = global::UI.Properties.Resources.shutdown_40px;
+            this.BtnExit.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnExit.Location = new System.Drawing.Point(252, 269);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(182, 43);
+            this.BtnExit.TabIndex = 0;
+            this.BtnExit.Text = "خروج";
+            this.BtnExit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnExit.Click += new System.EventHandler(this.BtnExit_Click);
+            // 
+            // BtnResturanRegister
+            // 
+            this.BtnResturanRegister.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnResturanRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnResturanRegister.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnResturanRegister.Image = global::UI.Properties.Resources.check_40px;
+            this.BtnResturanRegister.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnResturanRegister.Location = new System.Drawing.Point(252, 217);
+            this.BtnResturanRegister.Name = "BtnResturanRegister";
+            this.BtnResturanRegister.Size = new System.Drawing.Size(182, 43);
+            this.BtnResturanRegister.TabIndex = 0;
+            this.BtnResturanRegister.Text = "ثبت رستوران";
+            this.BtnResturanRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnResturanRegister.Click += new System.EventHandler(this.BtnResturanRegister_Click);
+            // 
+            // BtnMainResturan
+            // 
+            this.BtnMainResturan.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnMainResturan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMainResturan.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnMainResturan.Image = global::UI.Properties.Resources.ifood_40px;
+            this.BtnMainResturan.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnMainResturan.Location = new System.Drawing.Point(252, 53);
+            this.BtnMainResturan.Name = "BtnMainResturan";
+            this.BtnMainResturan.Size = new System.Drawing.Size(182, 43);
+            this.BtnMainResturan.TabIndex = 0;
+            this.BtnMainResturan.Text = "صفحه اصلی رستوران";
+            this.BtnMainResturan.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnMainResturan.Click += new System.EventHandler(this.BtnMainResturan_Click);
+            // 
+            // BtnMainUser
+            // 
+            this.BtnMainUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnMainUser.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnMainUser.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnMainUser.Image = global::UI.Properties.Resources.home_40px;
+            this.BtnMainUser.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnMainUser.Location = new System.Drawing.Point(252, 106);
+            this.BtnMainUser.Name = "BtnMainUser";
+            this.BtnMainUser.Size = new System.Drawing.Size(182, 43);
+            this.BtnMainUser.TabIndex = 0;
+            this.BtnMainUser.Text = "صفحه اصلی کاربران";
+            this.BtnMainUser.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnMainUser.Click += new System.EventHandler(this.BtnMainUser_Click);
+            // 
+            // BtnUserRegister
+            // 
+            this.BtnUserRegister.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnUserRegister.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnUserRegister.Font = new System.Drawing.Font("B Zar", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnUserRegister.Image = global::UI.Properties.Resources.add_fingerprint_40px;
+            this.BtnUserRegister.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnUserRegister.Location = new System.Drawing.Point(252, 162);
+            this.BtnUserRegister.Name = "BtnUserRegister";
+            this.BtnUserRegister.Size = new System.Drawing.Size(182, 43);
+            this.BtnUserRegister.TabIndex = 0;
+            this.BtnUserRegister.Text = "ثبت نام کاربر";
+            this.BtnUserRegister.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.BtnUserRegister.Click += new System.EventHandler(this.BtnUserRegister_Click);
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::UI.Properties.Resources._2_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(549, 340);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("B Zar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label BtnExit;
+        private Label BtnResturanRegister;
+        private Label BtnUserRegister;
+        private Label BtnMainResturan;
+        private Label BtnMainUser;
+    }
+}
