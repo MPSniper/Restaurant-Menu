316a7d0b19b4b4f24df3d917cf21ed2ea7ce3b1d
Kiana-Kiani <kiana8kiani@gmail.com>
2023-06-06 14:37:34 +0330

    Complete Main form

--- RestaurantMenu/UI/SplashForm.cs
+++ RestaurantMenu/UI/SplashForm.cs
@@ -9,11 +9,15 @@
 
         private void BtnUserRegister_Click(object sender, EventArgs e)
         {
+            this.Hide();
+
             new UserRegisterForm().ShowDialog();
         }
 
         private void BtnResturanRegister_Click(object sender, EventArgs e)
         {
+            this.Hide();
+
             new ResturanRegisterForm().ShowDialog();
         }
 
@@ -24,12 +28,20 @@
 
         private void BtnMainResturan_Click(object sender, EventArgs e)
         {
+            this.Hide();
             new MainResturanForm().ShowDialog();
         }
 
         private void BtnMainUser_Click(object sender, EventArgs e)
         {
+            this.Hide();
+
             new MainUserForm().ShowDialog();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
