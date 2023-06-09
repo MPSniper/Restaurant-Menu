fe2339ed419272b6300673119ebb97eda33c5440
Maryam <shatergonbad@gmail.com>
2023-06-09 19:14:38 +0330

    Add FoodViewModel Class To Service Libary

---
+++ RestaurantMenu/Service/ViewModels/FoodCartViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Service.ViewModels
+{
+    public class FoodCartViewModel
+    {
+        public FoodCartViewModel(int foodId, string foodName, decimal foodPrice, int count)
+        {
+            FoodId = foodId;
+            FoodName = foodName;
+            FoodPrice = foodPrice;
+            Count = count;
+        }
+
+        public int FoodId { get; set; }
+        public string FoodName { get; set; }
+        public decimal FoodPrice { get; set; }
+        public int Count { get; set; }
+    }
+}
