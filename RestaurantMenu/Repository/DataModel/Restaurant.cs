496afaf6599acf4346e07ea9cfd81179d690a17a
Mohammad Pourbarfeh <40278506+MPSniper@users.noreply.github.com>
2023-06-12 05:10:16 +0330

    Fixed Issue with saving NationalCode & Password Vise versa.

--- RestaurantMenu/Repository/DataModel/Restaurant.cs
+++ RestaurantMenu/Repository/DataModel/Restaurant.cs
@@ -8,15 +8,15 @@ namespace Repository.DataModel
         public string RestaurantName, OwnerName, strStartTime, strEndTime, ResAddress, NationalCode, Password;
         public int Id;
         public Restaurant() {}
-        public Restaurant(string RestaurantName, string OwnerName, string strStartTime, string strEndTime, string ResAddress, string NationalCode, string Password)
+        public Restaurant(string RestaurantName, string OwnerName, string strStartTime, string strEndTime, string ResAddress, string Password, string NationalCode)
         {
             this.RestaurantName = RestaurantName;
             this.OwnerName = OwnerName;
             this.strStartTime = strStartTime;
             this.strEndTime = strEndTime;
             this.ResAddress = ResAddress;
-            this.NationalCode = NationalCode;
             this.Password = Password;
+            this.NationalCode = NationalCode;
         }
         public int BtnSignUp()
         {
