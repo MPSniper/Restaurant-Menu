00471f0f0acb36b0722e511de57f518d8a80a361
Mohammad Pourbarfeh <40278506+MPSniper@users.noreply.github.com>
2023-06-12 04:00:44 +0330

    Added Connection file to DTO

---
+++ RestaurantMenu/Service/ConnectToDB.cs
@@ -0,0 +1,96 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Service
+{
+    public class ConnectToDB
+    {
+        private static SqlConnection sqlConnection = new SqlConnection();
+        private static SqlCommand command = new SqlCommand();
+        private static SqlDataAdapter adapter = new SqlDataAdapter();
+
+        //public static string strConnString = "Data Source=KODI\\MSSQLSERVER02;Initial Catalog=RestaurantDB;Integrated Security=True";
+       // public static string strConnString = "Data Source=.;Initial Catalog=RestaurantDB;Integrated Security=True";
+        public static string strConnString = "Data Source=MamadPC;Initial Catalog=NewRestaurant;Integrated Security=True";
+
+
+
+        public int ExecuteQuery(SqlCommand dbCommand)
+        {
+            if (sqlConnection.State == 0)
+            {
+                MessageBox.Show("00");
+                CreateConnection();
+            }
+            if (sqlConnection.State != 0)
+                MessageBox.Show("111");
+
+            try
+            {
+                if (sqlConnection.State == 0)
+                {
+                    CreateConnection();
+                }
+
+                dbCommand.Connection = sqlConnection;
+                dbCommand.CommandType = CommandType.Text;
+
+
+                return dbCommand.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public SqlConnection? CreateConnection()
+        {
+            try
+            {
+                if (sqlConnection.State != ConnectionState.Open)
+                {
+                    sqlConnection.ConnectionString = strConnString;
+                    sqlConnection.Open();
+                }
+                return sqlConnection;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "خطا در ارتباط با دیتابیس", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public void readData(SqlCommand  query, DataTable tblName)
+        {
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                {
+                    CreateConnection();
+                }
+
+                command = query;
+                command.CommandType = CommandType.Text;
+
+                adapter = new SqlDataAdapter(command);
+                adapter.Fill(tblName);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+    }
+}
