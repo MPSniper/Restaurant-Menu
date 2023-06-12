bcd0fda7e3a7b1ba71f4f7b4a9bdda49d4c89cad
Mohammad Pourbarfeh <40278506+MPSniper@users.noreply.github.com>
2023-06-12 05:30:55 +0330

    Move Factors_DTO from Data_Model to DTO folder in Service library

--- RestaurantMenu/Repository/DataModel/Factors.cs
+++ RestaurantMenu/Repository/DataModel/Factors.cs
@@ -1,17 +1,15 @@
-﻿using System.Data;
-using System.Data.SqlClient;
+﻿using Service.DTO;
+using System.Data;
 
 namespace Repository.DataModel
 {
     public class Factors
     {
-        ConnectToDB connect = new ConnectToDB();
+        FactorsDTO dto = new FactorsDTO();
         public int ID, UserKey, RestaurantKey;
         public decimal Sum;
 
-        public Factors()
-        {
-        }
+        public Factors() {}
         public Factors(int userKey, int restaurantKey, decimal sum)
         {
             UserKey = userKey;
@@ -21,47 +19,12 @@ namespace Repository.DataModel
 
         public int InsertFactor()
         {
-            try
-            {
-                SqlConnection? connection = connect.CreateConnection();
-                if (connection is null)
-                {
-                    return -1;
-                }
-                SqlCommand command = new SqlCommand("insert into Factor_Table(UserKey, RestaurantKey, [Sum]) values(@userKey, @restaurantKey, @factorSum)", connection);
-                command.Parameters.AddWithValue("@userKey", UserKey);
-                command.Parameters.AddWithValue("@restaurantKey", RestaurantKey);
-                command.Parameters.AddWithValue("@factorSum", Sum);
-                var result = command.ExecuteNonQuery();
-                connect.CloseConnection();
-                return result;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "خطا در ارتباط با دیتابیس", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return -1;
-            }
+            return dto.InsertFactor(UserKey, RestaurantKey, Sum);
         }
 
-        public (DataTable , string ) ShowFactors(int ID)
+        public (DataTable, string) ShowFactors(int ID)
         {
-            DataTable dt = new DataTable();
-            SqlConnection? con = connect.CreateConnection();
-
-            SqlCommand sqlcommand = new SqlCommand("SELECT Factor_Table.Sum, Restaurant_Table.RestaurantName, User_Table.FirstName FROM Factor_Table JOIN Restaurant_Table ON Factor_Table.RestaurantKey = Restaurant_Table.ID JOIN User_Table ON User_Table.ID = Factor_Table.Userkey WHERE RestaurantKey = @ID ", con);
-            sqlcommand.Parameters.AddWithValue("@ID", ID);
-
-
-            connect.readData(sqlcommand, dt);
-            DataColumn column = dt.Columns["Sum"];
-            DataRow[] rows = dt.Select();
-            var sum = 0;
-            foreach (DataRow row in rows)
-            {
-                object columnValue = row[column];
-                sum += Convert.ToInt32(columnValue);
-            }
-            return (dt, sum.ToString());
+            return dto.ShowFactors(ID);
         }
     }
 }
